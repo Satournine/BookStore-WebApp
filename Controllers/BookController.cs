31ebf20e75b6e33f5f7197e1ea1748435f3e50be
Satournine <tunaorkun@outlook.com>
2023-09-14 17:41:08 +0300

    Update BookController.cs

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -50,11 +50,33 @@ namespace WebApi.AddControllers
             var book = BookList.Where(x=> x.Id == id).SingleOrDefault();
             return book;
         }
-        //[HttpGet]
-        //public Book Get([FromQuery] string id)
-        //{
-        //   var book = BookList.Where(book => book.Id == Convert.ToInt32(id)).SingleOrDefault();
-        //   return book;
-        //}
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book newBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+            if (book is not null)
+            {
+                return BadRequest();
+            }
+            BookList.Add(newBook);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+            if (book is null)
+            {
+                return BadRequest();
+            }
+            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+            book.Genre = updatedBook.Genre != default ? updatedBook.Genre : book.Genre;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+
+            return Ok();
+        }
     }
 }
